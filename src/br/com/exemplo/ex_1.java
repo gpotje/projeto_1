72b0ec3de0b4f0287e3b5086300339cb8fae4f09
gpotje <gpotje@github.com>
2022-12-13 19:53:15 -0300

    exemplo 1

---
+++ src/br/com/exemplo/ex_1.java
@@ -0,0 +1,27 @@
+package br.com.exemplo;
+
+public class ex_1 {
+	
+	//tipo primitivos e não primitivos
+
+	public static void main(String[] args) {
+		
+		 int varInt = 0;
+		 long varLong = 1;
+		 char varChar = '4';
+		 float varFloat = 10000.1f;
+		 	 
+		 Integer varInteger = 1;
+		 Long varLongM = 1l;
+		 String varString = "Erika daskjdksnkan";
+		 Double varDouble =  10000.1;
+		 
+		 
+		 System.out.println("tipo primitivo: " + varInt + " tipo não primitivo: " + varInteger);
+		 System.out.println("tipo primitivo: " + varLong + " tipo não primitivo: " + varLongM);
+		 System.out.println("tipo primitivo: " + varChar + " tipo não primitivo: " + varString);
+		 System.out.println("tipo primitivo: " + varFloat + " tipo não primitivo: " + varDouble);
+
+	}
+
+}
